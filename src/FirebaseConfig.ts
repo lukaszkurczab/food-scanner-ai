586b99353adb60076f622d3b75256cda3b17d82f
Lukasz Kurczab <lukasz.kurczab@gmail.com>
2026-02-25 22:31:33 +0100

    firebaseConfig published

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,35 @@
+import { initializeApp, getApp, getApps } from "@react-native-firebase/app";
+import { getAuth } from "@react-native-firebase/auth";
+import { getFirestore } from "@react-native-firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMx2jGfr3mslwuu7PXwRry8M72794NMek",
+  authDomain: "calories-calculator-ai.firebaseapp.com",
+  projectId: "calories-calculator-ai",
+  storageBucket: "calories-calculator-ai.appspot.com",
+  messagingSenderId: "516060318021",
+  appId: "1:516060318021:web:f2413698c906624376d62c",
+  measurementId: "G-QNQBTJVFEW",
+};
+
+type FirebaseApp = ReturnType<typeof getApp>;
+
+let appPromise: Promise<FirebaseApp>;
+
+if (!getApps().length) {
+  appPromise = initializeApp(firebaseConfig);
+} else {
+  appPromise = Promise.resolve(getApp());
+}
+
+export const getFirebaseApp = () => appPromise;
+
+export const getFirebaseAuth = async () => {
+  const app = await appPromise;
+  return getAuth(app);
+};
+
+export const getFirebaseFirestore = async () => {
+  const app = await appPromise;
+  return getFirestore(app);
+};
