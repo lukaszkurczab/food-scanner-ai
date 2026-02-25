524fca4cce23a953a0628c6a7aa61548050cdd50
Lukasz Kurczab <lukasz.kurczab@gmail.com>
2026-02-25 14:04:46 +0100

    tests setup

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: "react-native",
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["<rootDir>/src/**/*.test.ts?(x)"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@assets/(.*)$": "<rootDir>/assets/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@contexts/(.*)$": "<rootDir>/src/context/$1",
+    "^@feature/(.*)$": "<rootDir>/src/feature/$1",
+    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@navigation/(.*)$": "<rootDir>/src/navigation/$1",
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "^@theme/(.*)$": "<rootDir>/src/theme/$1",
+    "^@types/(.*)$": "<rootDir>/src/types/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+  },
+  collectCoverageFrom: ["src/hooks/useChatHistory.ts", "src/hooks/useMeals.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  clearMocks: true,
+};
